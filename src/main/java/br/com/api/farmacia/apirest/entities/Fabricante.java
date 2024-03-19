09efb5e09a380fefb58cf005cc78e7a6c3e8b9dd
Fabiano <fabiano.ssantos79@gmail.com>
2024-03-19 02:40:22 -0300

    4 - Criar a Classe de Produto e Fabricante

---
+++ src/main/java/br/com/api/farmacia/apirest/entities/Fabricante.java
@@ -0,0 +1,6 @@
+package br.com.api.farmacia.apirest.entities;
+
+public class Fabricante {
+    private Long id;
+    private String nome;
+}
---
+++ src/main/java/br/com/api/farmacia/apirest/entities/Produto.java
@@ -0,0 +1,10 @@
+package br.com.api.farmacia.apirest.entities;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private Fabricante fabricante;
+}
