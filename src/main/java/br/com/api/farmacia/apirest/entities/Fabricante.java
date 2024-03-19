aed9833b95054988eafad084b015f16051623b43
Fabiano <fabiano.ssantos79@gmail.com>
2024-03-19 02:55:33 -0300

    5 - Mapear as Entidades para Tabelas

--- src/main/java/br/com/api/farmacia/apirest/entities/Fabricante.java
+++ src/main/java/br/com/api/farmacia/apirest/entities/Fabricante.java
@@ -1,6 +1,13 @@
 package br.com.api.farmacia.apirest.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="fabricantes")
 public class Fabricante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 }
--- src/main/java/br/com/api/farmacia/apirest/entities/Produto.java
+++ src/main/java/br/com/api/farmacia/apirest/entities/Produto.java
@@ -1,10 +1,17 @@
 package br.com.api.farmacia.apirest.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="produtos")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
     private Double preco;
+    @ManyToOne
     private Fabricante fabricante;
 }
